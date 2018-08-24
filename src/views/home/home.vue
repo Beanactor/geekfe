6c3a94afe9900f1ec97840a128c3f43f18526d5b
MrLss <18237115038@163.com>
2018-08-24 18:39:01 +0800

    构建view文件夹

---
+++ src/views/home/home.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="section-home">
+    <div class="projectDetail" @click='toProjectDetail'>
+
+    </div>
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  // 创建之前
+  beforeCreate: function() {},
+  // 创建之后
+  created() {
+    console.log(this.$api)
+    this.$axios(this.$api.eduList).then(
+      ({ rows }) => {
+        // console.log(rows);
+      },
+      (code, msg) => {
+        // 当有特定 code 需要特殊处理，传入 noShowDefaultError:true，在这个回调处理就行
+      }
+    );
+  },
+  // 挂载之前
+  beforeMount: function() {},
+  // 挂载之后
+  mounted: function() {
+ 
+  },
+  beforeDestroy: function() {
+  },
+  methods: {
+    toProjectDetail() {
+      this.$router.push({
+        path: '/projectDetail'
+      })
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.section-home {
+  color: #f00;
+  .projectDetail {
+    width: 100px;
+    height: 100px;
+    background-color: #08f;
+  }
+}
+</style>
+
+
+
---
+++ src/views/project/projectDetail.vue
@@ -0,0 +1,41 @@
+<template>
+ <div class='project-details'>
+
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+     }
+   },
+   // 创建之前
+   beforeCreate() {
+
+   },
+   // 创建之后
+   created() {
+   },
+   // 挂载之前
+   beforeMount() {
+
+   },
+   // 挂载之后
+   mounted() {
+
+   },
+   //激活之后
+   activated() {
+
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
