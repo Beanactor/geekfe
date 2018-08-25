906647df7b204d8b8c5cec11f485353e25c3609c
MrLss <18237115038@163.com>
2018-08-25 14:25:49 +0800

    添加rem注释

---
+++ src/utils/rem.js
@@ -0,0 +1,13 @@
+// 屏幕等分为 6/1 即 6 份; 1rem = clientWidth/6 px ;
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 1 * (clientWidth / 6) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
